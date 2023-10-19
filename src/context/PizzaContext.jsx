ac47cd89505135ca3ca7452ea1a25283fbea7d40
Jotape <juanpa1999a@gmail.com>
2023-10-19 16:04:35 -0300

    Add pizzaCart to have an updatable cart object

--- src/context/PizzaContext.jsx
+++ src/context/PizzaContext.jsx
@@ -4,14 +4,18 @@ const API_PIZZA = "/pizzas.json";
 
 export const PizzaContext = createContext();
 
+
 const PizzaProvider = ({ children }) => {
     const [pizzas, setPizza] = useState(null);
     const [pizzaApi, setPizzaApi] = useState(API_PIZZA)
+    const [pizzaCart,setPizzaCart] = useState([])
 
     //Fetch
     useEffect(() => {
         fetchPizzas();
-      }, [ , pizzaApi]);
+        console.log("Paso 1")
+        console.log(pizzas)
+      }, []);
             
       const fetchPizzas = async ()=> {
         axios
@@ -24,8 +28,41 @@ const PizzaProvider = ({ children }) => {
           });
       };
 
+    //Shopping cart
+    useEffect(() => {
+        if(pizzas !== null){
+            cartMount(pizzas);
+            console.log("Eureka")
+            console.log(pizzaCart)
+            //Resultados
+            const example = () => {
+                let total = 0
+                pizzaCart.forEach((el) => {let result = el.price * el.quantity; total += result})
+                console.log(total)
+            } 
+            example()
+        }
+      }, [pizzas]);
+    
+
+    const cartMount = ( apiElement ) =>{
+        let cart = []
+        apiElement.map((el) => {
+            const container = {}
+
+            container.id = el.id
+            container.product = el.name;
+            container.price = el.price;
+            container.image = el.img;
+            container.quantity = 0
+            
+            cart.push(container)
+        })
+        setPizzaCart(cart)
+    }
+
     return (
-        <PizzaContext.Provider value={{ pizzas, setPizza, pizzaApi, setPizzaApi }}>
+        <PizzaContext.Provider value={{ pizzas, setPizza, pizzaApi, setPizzaApi, pizzaCart, setPizzaCart }}>
             {children}
         </PizzaContext.Provider>
     );
