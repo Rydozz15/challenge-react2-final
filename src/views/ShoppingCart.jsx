b969325a4ba3e2dcd1775c457b7499ed8b52c5a0
Jotape <juanpa1999a@gmail.com>
2023-10-20 22:32:00 -0300

    Add product render and function to add/subs items

---
+++ src/views/ShoppingCart.jsx
@@ -0,0 +1,45 @@
+import { useContext } from "react"
+import { PizzaContext } from "../context/PizzaContext"
+import "./ShoppingCart.css"
+
+const ShoppingCart = () =>{
+    const { pizzaCart, setPizzaCart } = useContext(PizzaContext)
+
+    const addSubtractCart = (id, action) => {
+        const copyCart = [...pizzaCart]
+        let chosenIndex = copyCart.findIndex((el) => el.id == id)
+        if (action == "add"){
+            copyCart[chosenIndex].quantity += 1
+        }
+        else if (action == "sub" && copyCart[chosenIndex].quantity > 0) {
+            copyCart[chosenIndex].quantity -= 1
+        }
+        setPizzaCart(copyCart)
+    }
+
+    return(
+        <section className="shopping-description">
+            {pizzaCart == [] ? ""
+                : pizzaCart.map((el) => 
+                        <article className="card-shopping" key={el.id}>
+                            <div className="card-product">
+                                <img src={el.image} alt="" />
+                                <h2>{el.product}</h2>
+                            </div>
+                            <div className="card-price">
+                                <h3 className="price">{el.price * el.quantity}</h3>
+                                <button onClick={() => addSubtractCart(el.id, "add")}>
+                                        +
+                                </button>
+                                <h2>{el.quantity}</h2>
+                                <button onClick={() => addSubtractCart(el.id, "sub")}>
+                                    -
+                                </button>
+                            </div>
+                    </article>
+                    )}
+        </section>
+    )
+}
+
+export default ShoppingCart
