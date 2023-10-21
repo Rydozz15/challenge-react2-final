82f583a580d73db15866ac53707597c3faaa1e17
Jotape <juanpa1999a@gmail.com>
2023-10-20 22:50:18 -0300

    Add Routes in App and Router Provider in main

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+//Context Provider
+import PizzaProvider from './context/PizzaContext'
+//Router DOM
+import { Routes, Route } from "react-router-dom";
+//Navbar
+import Navbar from "./components/Navbar"
+//Views
+import Home from './views/Home'
+import Description from './views/Description'
+import ShoppingCart from './views/ShoppingCart'
 import './App.css'
 
 function App() {
-
   return (
     <>
+    <PizzaProvider>
+      <Navbar />
+      <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/pizza/:type" element={<Description />}/>
+          <Route path="/cart" element={<ShoppingCart />}/>
+        </Routes>
+    </PizzaProvider>
     </>
   )
 }
